3abf38d349cff607fc25fbe72aa255f959b7672f
Aleksandr <aaleksandrnik2008@gmail.com>
2026-09-06 18:47:05 +0300

    Cosmetics work

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
@@ -18,13 +18,14 @@ class Data(StatesGroup):
 
 @router.message(Command("start"))
 async def start(message: Message):
-    await message.answer('Меню\n\n'
-                         'Выберите действие', reply_markup=keyboard_menu())
+    await message.answer('🥸Меню\n\n'
+                         'Выберите ниже действие☺️', reply_markup=keyboard_menu())
 
 @router.callback_query(F.data == "help")
 async def help(callback: CallbackQuery):
     await callback.answer()
-    await callback.message.edit_text(text='Это бот для подсчётов расходов. Чтобы добавить расходы'
+    await callback.message.edit_text(text='❓❓❓\n\n'
+                                          'Это бот для подсчётов расходов. Чтобы добавить расходы'
                                           'вам нужно нажать на кнопку "Добавить расходы".', reply_markup=keyboard_back_to_menu())
 
 @router.callback_query(F.data == "statistic")
@@ -35,7 +36,7 @@ async def statistics(callback: CallbackQuery):
     expenses = get_expenses(user_id)
 
     if not expenses:
-        await callback.message.edit_text(text='У вас пока что нет расходов', reply_markup=keyboard_back_to_menu())
+        await callback.message.edit_text(text='У вас пока что нет расходов😔', reply_markup=keyboard_back_to_menu())
         return
 
     total = 0
@@ -48,11 +49,11 @@ async def statistics(callback: CallbackQuery):
 
         categories[category] = categories.get(category, 0) + amount
 
-    text = (f"Всего: {total}")
+    text = (f"💰Общая сумма расходов: {total}\n\n")
 
     for category, amount in categories.items():
         name = category_name.get(category, category)
-        text += f"{name}: {amount:.2f}\n"
+        text += f"{name}: 🤑{amount:.2f}\n"
 
     await callback.message.edit_text(text=text, reply_markup=keyboard_back_to_menu())
 
@@ -60,27 +61,27 @@ async def statistics(callback: CallbackQuery):
 async def start_FSM(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
 
-    await callback.message.edit_text(text='Напишите ниже сумму трат')
+    await callback.message.edit_text(text='Напишите ниже сумму трат🤑')
 
     await state.set_state(Data.amount)
 
 @router.message(Data.amount)
 async def get_amount(message: Message, state: FSMContext):
     if not message.text.isdigit():
-        await message.answer("Введите расходы числом!")
+        await message.answer("Введите расходы числом!😡")
         return
 
     await state.update_data(amount=float(message.text))
-    await message.answer(text='Выберите категорию', reply_markup=keyboard_category())
+    await message.answer(text='Выберите категорию🤓', reply_markup=keyboard_category())
 
     await state.set_state(Data.category)
 
 category_name = {
-    "Food": "Еда",
-    "Transport": "Транспорт",
-    "Entertainment": "Развлечения",
-    "Purchases": "Покупки",
-    "Other": "Другое"
+    "Food": "🍔Еда",
+    "Transport": "🚖Транспорт",
+    "Entertainment": "🎭Развлечения",
+    "Purchases": "🛒Покупки",
+    "Others": "🔄Другое"
 }
 
 @router.callback_query(Data.category, F.data.startswith("category_"))
@@ -89,11 +90,9 @@ async def get_category(callback: CallbackQuery, state: FSMContext):
 
     category = callback.data.split("_")[1]
 
-    name = category_name.get(category)
+    await state.update_data(category=category)
 
-    await state.update_data(category=name)
-
-    await callback.message.edit_text(text='Добавьте заметку к расходам')
+    await callback.message.edit_text(text='Добавьте заметку к расходам🙃')
 
     await state.set_state(Data.comment)
 
@@ -110,9 +109,10 @@ async def get_comment(message: Message, state: FSMContext):
     add_expense(user_id, user_data['amount'], user_data['category'], user_data['comment'], created_at)
 
 
-    await message.answer(f"{user_data['amount']}\n"
-                        f"{user_data['category']}\n"
-                        f"{user_data['comment']}", reply_markup=keyboard_back_to_menu())
+    await message.answer(f"✅ Расходы сохранились.\n\n"
+                         f"💰Сумма: {user_data['amount']}\n"
+                        f"🤓Категория: {user_data['category']}\n"
+                        f"📆Дата: {user_data['comment']}", reply_markup=keyboard_back_to_menu())
 
     await state.clear()
 
@@ -124,26 +124,26 @@ async def history(callback: CallbackQuery):
     expenses = get_expenses(user_id)
 
     if not expenses:
-        await callback.message.edit_text(text="История расходов\n\n"
-                                         "У вас пока нет расходов.", reply_markup=keyboard_back_to_menu())
+        await callback.message.edit_text(text="⏳История расходов\n\n"
+                                         "У вас пока нет расходов.😞", reply_markup=keyboard_back_to_menu())
 
         return
 
-    text = "История расходов\n\n"
+    text = "⏳История расходов\n\n"
 
     for expense in expenses:
         expense_id, user_id, amount, category, comment, created_at = expense
 
-        name = category_name.get(category)
+        name = category_name.get(category, category)
 
-        text += f"{amount} - {name}\n"
-        text += f"{comment}\n"
-        text += f"{created_at}\n"
+        text += f"💰 {amount} - {name}\n"
+        text += f"😶Комментарий: {comment}\n"
+        text += f"📆Дата: {created_at}\n\n"
 
     await callback.message.edit_text(text=text, reply_markup=keyboard_back_to_menu())
 
 @router.callback_query(F.data == "back_to_menu")
 async def back_to_menu(callback: CallbackQuery):
     await callback.answer()
-    await callback.message.edit_text(text='Меню\n\n'
-                                          'Выберите действие', reply_markup=keyboard_menu())
+    await callback.message.edit_text(text='🥸Меню\n\n'
+                                          'Выберите действие☺️', reply_markup=keyboard_menu())
